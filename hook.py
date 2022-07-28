a716de6c58fe85615ce54a63df86348c51f6abc4
svennd <svenn.dhert@uantwerpen.be>
2022-07-28 15:30:14 +0200

    different method when working with tokens. (not the global key)

--- hook.py
+++ hook.py
@@ -40,8 +40,7 @@ else:
 
 try:
     CF_HEADERS = [{
-        'X-Auth-Email': e,
-        'X-Auth-Key'  : k,
+        'Authorization': 'Bearer ' + k,
         'Content-Type': 'application/json',
     } for e,k in zip(os.environ['CF_EMAIL'].split(), os.environ['CF_KEY'].split()) ]
 except KeyError:
